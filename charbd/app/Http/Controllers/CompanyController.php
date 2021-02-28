c8408951678de94e50f6af8b701ad4898940a93c
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-02-28 13:20:15 +0600

    Creat show and create api for company

--- charbd/app/Http/Controllers/CompanyController.php
+++ charbd/app/Http/Controllers/CompanyController.php
@@ -11,4 +11,14 @@ class CompanyController extends Controller
     {
        return Company::all();
     }
+
+    public function show($id) {
+        $company = Company::find($id);
+        return response($company);
+    }
+
+    public function store(Request $request) {
+        $company = Company::create($request->all());
+        return response($company);
+    }
 }
