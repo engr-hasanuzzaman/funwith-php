3ca7b2f90c6d5f07aef9542f98d52cc6d31907ec
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-03-05 01:13:36 +0600

    Create action for user profile data

--- charbd/app/Http/Controllers/UserController.php
+++ charbd/app/Http/Controllers/UserController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function index(Request $request)
     {
        return User::all();
+    }
+
+    public function profile()
+    {
+        return response(Auth::user());
     }
 }
