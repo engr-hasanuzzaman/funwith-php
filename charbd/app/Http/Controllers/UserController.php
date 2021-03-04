8348deae2fc87db25636e23762ad7364144b3b92
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-03-05 01:18:56 +0600

    Creat profile update method

--- charbd/app/Http/Controllers/UserController.php
+++ charbd/app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
@@ -16,5 +17,12 @@ class UserController extends Controller
     public function profile()
     {
         return response(Auth::user());
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $user = Auth::user();
+        $user->update($request->only('name'));
+        return response($user, Response::HTTP_OK);
     }
 }
