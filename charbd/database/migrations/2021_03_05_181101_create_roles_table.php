21bacc71294922d83bb50f1a3a790d04ebbe0e60
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-03-06 00:15:46 +0600

    Update the roles table create migration

--- charbd/database/migrations/2021_03_05_181101_create_roles_table.php
+++ charbd/database/migrations/2021_03_05_181101_create_roles_table.php
@@ -15,7 +15,7 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->name('string');
+            $table->string('name')->nullable(false);
             $table->timestamps();
         });
     }
