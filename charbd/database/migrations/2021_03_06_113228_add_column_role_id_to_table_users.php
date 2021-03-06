0b9599fabb54630de504ab7d847ea6fcb54387b0
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-03-06 22:27:20 +0600

    Make seeder for role
    - update user seeder with the random role

--- charbd/database/migrations/2021_03_06_113228_add_column_role_id_to_table_users.php
+++ charbd/database/migrations/2021_03_06_113228_add_column_role_id_to_table_users.php
@@ -14,8 +14,8 @@ class AddColumnRoleIdToTableUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('role_id')->nullable(true);
-            $table->foreign('role_id')->nullable(true)->references('id')->on('roles');
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');
         });
     }
 
--- charbd/database/seeders/DatabaseSeeder.php
+++ charbd/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $roleSeeder = new RoleSeeder();
+        $roleSeeder->run();
+        \App\Models\User::factory(10)->create();
     }
 }
