7600ba5cf6400637da506be284ce33fcdf100901
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-03-06 18:12:21 +0600

    Create migration for adding role_id foreign_key to users table

---
+++ charbd/database/migrations/2021_03_06_113228_add_column_role_id_to_table_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnRoleIdToTableUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id')->nullable(true);
+            $table->foreign('role_id')->nullable(true)->references('id')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role_id');
+            $table->dropForeign(['role_id']);
+        });
+    }
+}
