c78c662556bd30d33387f32e6b3e088d42cd7e0f
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-03-07 10:43:55 +0600

    Adding product stor method on going

--- charbd/app/Http/Controllers/ProductController.php
+++ charbd/app/Http/Controllers/ProductController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
@@ -26,7 +29,15 @@ class ProductController extends Controller
 
     public function update(Request $request)
     {
-        
+        $image = $request->file('image');
+        $image_name = Str::random(10);
+        $image_path = Storage::putFileAs('imgages', $image, $image_name . '.' . $image->extension());
+        $product = Product::create($request->only('title', 'description', 'price') + [ 'image' => env('APP_URL') . $image_path]);
+        // dump($image_name);
+        // dump($image_path);
+        // Log::channel('single')->debug("this is my log message");
+        // return response($product, Response::HTTP_CREATED);
+        return $image_path . $image_name;
     }
 
     public function destroy(int $id)
