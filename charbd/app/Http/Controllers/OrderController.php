e9aac2ca1d0b3836b2259cfcf841e691b63b5796
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-03-09 22:32:00 +0600

    Write action export for exporting the order as CSV file

--- charbd/app/Http/Controllers/OrderController.php
+++ charbd/app/Http/Controllers/OrderController.php
@@ -19,4 +19,36 @@ class OrderController extends Controller
         return response(new OrderResource(Order::find($id)), Response::HTTP_OK);
     }
 
+    public function export()
+    {
+        $headers = [
+            'Content-Type' => 'text/csv',
+            'Content-Disposition' => 'attachments; filename=orders.csv',
+            'Pragma' => 'no-cachee',
+            'Cache-Control' => 'must-revalidate, post-check=0, pre-check=0',
+            'Expires' => '0'
+        ];
+
+        $callback = function () {
+            $orders = Order::all();
+            $file = fopen("php::/output", "w");
+            
+            // header row
+            fputcsv($file, ['ID', 'Name', 'Email', 'Product Title', 'Price', 'Quantity']);
+
+            // body
+            foreach($orders as $order) {
+                fputcsv($file, [$order->id, $order->name, $order->email, "", "", '']);
+
+                foreach($order->orderItems as $item) {
+                    fputcsv($file, ['', '', '', $item->product_title, $item->price, $item->quantity]);
+                }
+            }
+
+            fclose($file);
+        };
+
+        return \Response::stream($callback, 200, $headers);
+    }
+
 }
