cfa12b7ecdef3fe156472ed7dd3c1460cbaf7b15
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-03-11 01:41:29 +0600

    Seeder for role permission

--- charbd/database/seeders/RolePermissionSeeder.php
+++ charbd/database/seeders/RolePermissionSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Permission;
 use App\Models\Role;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RolePermissionSeeder extends Seeder
 {
@@ -14,8 +16,36 @@ class RolePermissionSeeder extends Seeder
      */
     public function run()
     {
+        $permissions = Permission::all();
         $admin = Role::whereName('Admin')->first();
+        foreach($permissions as $permission) {
+            DB::table('permission_role')->insert([
+                'role_id' => $admin->id,
+                'permission_id' => $permission->id,
+            ]);
+        }
+
+        // TODO: need to change manager to 
         $manager = Role::whereName('Manager')->first();
         $editor = Role::whereName('Editor')->first();
+        foreach($permissions as $permission) {
+            if (!in_array($permission->name, ['edit_roles'])) {
+                DB::table('permission_role')->insert([
+                    'role_id' => $manager->id,
+                    'permission_id' => $permission->id,
+                ]);
+            }
+        }
+
+         // all the permissions except edit_roles, edit_users
+         $editor = Role::whereName('Editor')->first();
+         foreach($permissions as $permission) {
+             if (!in_array($permission->name, ['edit_roles', 'edit_users'])) {
+                 DB::table('permission_role')->insert([
+                     'role_id' => $manager->id,
+                     'permission_id' => $permission->id,
+                 ]);
+             }
+         }
     }
 }
