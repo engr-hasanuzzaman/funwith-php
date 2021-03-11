7e8bc33d56e213fa6f90fc1759f45b06a9b9bb60
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-03-12 01:10:44 +0600

    Allow role controller to handle permissions

--- charbd/app/Http/Controllers/RoleController.php
+++ charbd/app/Http/Controllers/RoleController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Resources\RoleResource;
 use App\Models\Role;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
 
 class RoleController extends Controller
@@ -27,8 +28,19 @@ class RoleController extends Controller
      */
     public function store(Request $request)
     {
+        DB::beginTransaction();
         $role = Role::create($request->only('name'));
+        if($permissionIds = $request->input('permissions')) {
+            foreach($permissionIds as $permissionId) {
+                DB::table('permission_role')->insert([
+                    'permission_id' => $permissionId,
+                    'role_id' => $role->id,
+                ]);
+            }
+        }
+        DB::commit();
         return response(new RoleResource($role), HttpFoundationResponse::HTTP_CREATED);
+
     }
 
     /**
@@ -51,8 +63,20 @@ class RoleController extends Controller
      */
     public function update(Request $request, $id)
     {
+        DB::beginTransaction();
         $role = Role::find($id);
         $role->update($request->only('name'));
+        if($permissionIds = $request->input('permissions')) {
+            // remove old permissions
+            DB::table('permission_role')->where('role_id', $role->id)->delete();
+            foreach($permissionIds as $permissionId) {
+                DB::table('permission_role')->insert([
+                    'permission_id' => $permissionId,
+                    'role_id' => $role->id,
+                ]);
+            }
+        }
+        DB::commit();
         return response(new RoleResource($role), HttpFoundationResponse::HTTP_ACCEPTED);
     }
 
@@ -64,7 +88,11 @@ class RoleController extends Controller
      */
     public function destroy($id)
     {
+        DB::beginTransaction();
+        // delete permissions also
+        DB::table('permission_role')->where('role_id', $id)->delete();
         Role::destroy($id);
+        DB::commit();
         return response(null, HttpFoundationResponse::HTTP_NO_CONTENT);
     }
 }
