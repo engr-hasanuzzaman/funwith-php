f8bf861e190739336f2cfb9cce8bf43250b3051e
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-03-13 02:07:09 +0600

    Apply authorization to the chart action

--- charbd/app/Http/Controllers/DashboardController.php
+++ charbd/app/Http/Controllers/DashboardController.php
@@ -2,19 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ChartResource;
 use App\Models\Order;
+use Gate;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function chart()
     {
+        Gate::authorize('view', 'orders');
         $orders = Order::query()
             ->join('order_items', 'orders.id', '=', 'order_items.order_id')
-            ->selectRaw("DATE_FORMAT(orders.created_at, '%Y') as date, sum(order_items.price*order_items.quantity) as sum")
+            ->selectRaw("DATE_FORMAT(orders.created_at, '%Y-%m-%d') as date, sum(order_items.price*order_items.quantity) as sum")
             ->groupBy('date')
             ->get();
         
-        return $orders;
+        return ChartResource::collection($orders);
     }
 }
---
+++ charbd/app/Http/Resources/ChartResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ChartResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'sum' => (float)$this->sum,
+            'date' => $this->date,
+        ];
+    }
+}
