c11819aac88ca65072f8ea600fe7ba7acad66342
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-03-16 01:58:05 +0600

    Separate menu and header in differen componets

--- charbd/charbd_frontend/src/App.vue
+++ charbd/charbd_frontend/src/App.vue
@@ -1,4 +1,49 @@
 <template>
-  <h1>hello charbd</h1>
+  <div>
+    <Header />
+
+    <div class="container-fluid">
+      <div class="row">
+        <Menu />
+        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+          <h2>Section title</h2>
+          <div class="table-responsive">
+            <table class="table table-striped table-sm">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Header</th>
+                  <th>Header</th>
+                  <th>Header</th>
+                  <th>Header</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1,001</td>
+                  <td>random</td>
+                  <td>data</td>
+                  <td>placeholder</td>
+                  <td>text</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </main>
+      </div>
+    </div>
+  </div>
 </template>
 
+<script>
+import Menu from "@/components/Menu";
+import Header from "@/components/Header";
+
+export default {
+  name: "App",
+  components: {
+    Menu,
+    Header
+  },
+};
+</script>
