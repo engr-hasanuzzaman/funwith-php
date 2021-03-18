b0ad469ef104946b5daae32920ac482cbfa062e0
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-03-19 00:57:24 +0600

    Add pagination functionality on the users page

--- charbd/charbd_frontend/src/secure/user/Users.vue
+++ charbd/charbd_frontend/src/secure/user/Users.vue
@@ -11,13 +11,23 @@
       </thead>
       <tbody>
         <tr v-for="user in users" :key="user.id">
-          <td>{{user.name}}</td>
-          <td>{{user.email}}</td>
-          <td>{{user.role.name}}</td>
+          <td>{{ user.name }}</td>
+          <td>{{ user.email }}</td>
+          <td>{{ user.role.name }}</td>
           <td>...</td>
         </tr>
       </tbody>
     </table>
+    <nav class="navbar navbar-expand">
+      <ul class="nav">
+        <li class="nav-item">
+          <a href="javascript:void(0)" class="nav-link" @click="prevPage">Previoust</a>
+        </li>
+        <li class="nav-item">
+          <a href="javascript:void(0)" class="nav-link" @click="nextPage">Next</a>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
@@ -29,14 +39,36 @@ export default {
   name: "Users",
   setup() {
     const users = ref([]);
-
+    const links = ref({});
+    
     onMounted(async () => {
       const response = await axios.get("users");
       users.value = response.data.data;
+      links.value = response.data.links;
+      debugger;
     });
 
+    const nextPage = async () => {
+        debugger;
+        if (!links.value.next)
+            return;
+        const response = await axios.get(links.value.next);
+        users.value = response.data.data;
+        links.value = response.data.links;
+    };
+
+    const prevPage = async () => {
+        if (!links.value.prev)
+            return;
+        const response = await axios.get(links.value.prev);
+        users.value = response.data.data;
+        links.value = response.data.links;
+    };
     return {
       users,
+      links,
+      nextPage,
+      prevPage
     };
   },
 };
