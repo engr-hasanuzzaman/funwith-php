022c091c2d9daba06a69cfaf67334e3d80d26bb5
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-03-22 01:04:43 +0600

    Route for new user

--- charbd/charbd_frontend/src/router/index.ts
+++ charbd/charbd_frontend/src/router/index.ts
@@ -5,17 +5,19 @@ import Profile from "@/secure/Profile.vue";
 import Secure from "@/secure/Secure.vue";
 import Dashboard from "@/secure/dashboard/Dashboard.vue";
 import Users from "@/secure/user/Users.vue";
+import UserCreate from "@/secure/user/UserCreate.vue";
 
 const routes: Array<RouteRecordRaw> = [
   { path: '/register', component: Register },
   { path: '/login', component: Login },
-  
+
   {
     path: '/',
     component: Secure,
     children: [
       { path: '/profile', component: Profile },
       { path: '/users', component: Users },
+      { path: '/users/new', component: UserCreate },
       { path: '', component: Dashboard },
     ]
   },
