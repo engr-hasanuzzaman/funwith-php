50cca705d487ff0cb147fb0057eda9be14cb555c
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-03-22 01:01:21 +0600

    Create form for new user

---
+++ charbd/charbd_frontend/src/secure/user/UserCreate.vue
@@ -0,0 +1,57 @@
+<template>
+  <form action="" method="post">
+    <div class="form-group">
+      <label for="name">Name</label>
+      <input type="text" placeholder="name" id="name" class="form-control" v-model="name"/>
+    </div>
+    <div class="form-group">
+      <label for="email">Name</label>
+      <input type="email" placeholder="email" id="email" class="form-control"  v-model="email"/>
+    </div>
+    <div class="form-group">
+      <label for="password">Name</label>
+      <input
+        type="password"
+        placeholder="password"
+        id="password"
+        class="form-control"
+         v-model="password"
+      />
+    </div>
+    <div class="form-group">
+      <label for="confirm-password">Name</label>
+      <input
+        type="password"
+        placeholder="confirm password"
+        id="confirm-password"
+        class="form-control"
+         v-model="confirmPassword"
+      />
+    </div>
+  </form>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity';
+export default {
+  name: 'UserCreate',
+  setup() {
+    const name = ref('');
+    const email = ref('');
+    const roleId = ref(0);
+    const password = ref('');
+    const confirmPassword = ref('');
+
+    return {
+      name,
+      email,
+      roleId,
+      password,
+      confirmPassword
+    }
+  }
+};
+</script>
+
+<style>
+</style>
