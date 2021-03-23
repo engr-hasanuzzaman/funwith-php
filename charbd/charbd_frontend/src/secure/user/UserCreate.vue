f890c973ad3eafc5797469b13e37440a3b9ce655
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-03-23 07:47:15 +0600

    Update the user crete component

--- charbd/charbd_frontend/src/secure/user/UserCreate.vue
+++ charbd/charbd_frontend/src/secure/user/UserCreate.vue
@@ -2,17 +2,32 @@
   <form @submit.prevent="submitHandler">
     <div class="form-group">
       <label for="name">Name</label>
-      <input type="text" placeholder="name" id="name" class="form-control" v-model="name" required/>
+      <input
+        type="text"
+        placeholder="name"
+        id="name"
+        class="form-control"
+        v-model="name"
+        required
+      />
     </div>
     <div class="form-group">
       <label for="email">Email</label>
-      <input type="email" placeholder="email" id="email" class="form-control"  v-model="email"/>
+      <input
+        type="email"
+        placeholder="email"
+        id="email"
+        class="form-control"
+        v-model="email"
+      />
     </div>
     <div class="form-group">
       <label for="roleid">Role</label>
       <select name="role_id" id="roleid" class="form-control" required>
         <option value="">Select a role</option>
-        <option v-for="role in roles" :value="role.i" :key="role.id">{{ role.name }}</option>
+        <option v-for="role in roles" :value="role.id" :key="role.id">
+          {{ role.name }}
+        </option>
       </select>
     </div>
     <div class="form-group">
@@ -22,7 +37,7 @@
         placeholder="password"
         id="password"
         class="form-control"
-         v-model="password"
+        v-model="password"
       />
     </div>
     <div class="form-group">
@@ -32,45 +47,48 @@
         placeholder="confirm password"
         id="confirm-password"
         class="form-control"
-         v-model="confirmPassword"
+        v-model="confirmPassword"
       />
+    </div>
+    <div class="form-group">
+      <button type="submit" class="form-control">Submit</button>
     </div>
   </form>
 </template>
 
 <script lang="ts">
-import { ref } from '@vue/reactivity';
-import { onMounted } from '@vue/runtime-core';
-import axios from 'axios';
-import { Role } from '@/classes/role';
+import { ref } from "@vue/reactivity";
+import { onMounted } from "@vue/runtime-core";
+import axios from "axios";
+import { Role } from "@/classes/role";
 
 export default {
-  name: 'UserCreate',
+  name: "UserCreate",
   setup() {
-    const name = ref('');
-    const email = ref('');
+    const name = ref("");
+    const email = ref("");
     const roleId = ref(0);
-    const password = ref('');
-    const confirmPassword = ref('');
-    
+    const password = ref("");
+    const confirmPassword = ref("");
+
     // fetch the role ids
     const roles = ref([] as Role[]);
     onMounted(async () => {
-      const response = await axios.get('roles');
+      const response = await axios.get("roles");
       roles.value = response.data.data as Role[];
     });
 
     // create a new user
     const submitHandler = async () => {
-      const response = await axios.post('users', {
+      const response = await axios.post("users", {
         name: name.value,
         email: email.value,
         role_id: roleId.value,
         password: password.value,
-        confirm_password: confirmPassword.value
+        confirm_password: confirmPassword.value,
       });
     };
- 
+
     return {
       name,
       email,
@@ -78,9 +96,9 @@ export default {
       password,
       confirmPassword,
       roles,
-      submitHandler
-    }
-  }
+      submitHandler,
+    };
+  },
 };
 </script>
 
