745d5acd229cab2fc62ffd8071eb87ec1016b020
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-03-25 01:01:07 +0600

    Setup the route for the roles
    - display list of roles and delete functionality

--- charbd/charbd_frontend/src/router/index.ts
+++ charbd/charbd_frontend/src/router/index.ts
@@ -5,6 +5,7 @@ import Profile from "@/secure/Profile.vue";
 import Secure from "@/secure/Secure.vue";
 import Dashboard from "@/secure/dashboard/Dashboard.vue";
 import Users from "@/secure/user/Users.vue";
+import Roles from "@/secure/role/Roles.vue";
 import UserCreate from "@/secure/user/UserCreate.vue";
 import UserEdit from "@/secure/user/UserEdit.vue";
 
@@ -20,6 +21,9 @@ const routes: Array<RouteRecordRaw> = [
       { path: '/users', component: Users },
       { path: '/users/new', component: UserCreate },
       { path: '/users/:id/edit', component: UserEdit },
+      { path: '/roles', component: Roles },
+      { path: '/roles/new', component: UserCreate },
+      { path: '/roles/:id/edit', component: UserEdit },
       { path: '', component: Dashboard },
     ]
   },
