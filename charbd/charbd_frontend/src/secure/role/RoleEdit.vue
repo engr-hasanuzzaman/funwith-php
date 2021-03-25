ef0416cbffe51485dcce41f22163f855dde1d833
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-03-25 23:57:51 +0600

    Use checkbox for selecing permissions instead of multiple select option on the edit component

--- charbd/charbd_frontend/src/secure/role/RoleEdit.vue
+++ charbd/charbd_frontend/src/secure/role/RoleEdit.vue
@@ -13,12 +13,10 @@
     </div>
     <div class="form-group">
       <label for="roleid">Permissions</label>
-      <select name="role_id" id="roleid" class="form-control" v-model="permissionIds" multiple>
-        <option value="0">Select Permissions</option>
-        <option v-for="permission in permissions" :value="permission.id" :key="permission.id">
-          {{ permission.name }}
-        </option>
-      </select>
+      <template v-for="permission in permissions" :value="permission.id" :key="permission.id">
+         <label class="mr-1" :for="permission.id">{{ permission.name }}</label>
+         <input class="mr-4" :id="permission.id" type="checkbox" :value="permission.id" v-model="permissionIds">
+      </template>
     </div>
     <div class="form-group">
       <button type="submit" class="form-control">Submit</button>
