7cbf9d530621e30a8f4a9f22b57f134212b4e5e9
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-03-27 09:35:33 +0600

    Update the product class definition

--- charbd/charbd_frontend/src/classes/product.ts
+++ charbd/charbd_frontend/src/classes/product.ts
@@ -5,12 +5,13 @@ export class Product implements Entity {
     title: string;
     description: string;
     image_url: string;
+    price: number;
 
-    constructor(id = 0, title = '', description = '', image = '') {
+    constructor({id = 0, title = '', description = '', image_url = '', price = 0}) {
         this.id = id;
         this.title = title;
         this.description = description;
-        this.image_url = image;
+        this.image_url = image_url;
+        this.price = price;
     }
-    
 }
