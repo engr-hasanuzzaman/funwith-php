c49471cb03293c1be2bfc9279836e0afd0e9f037
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-04-02 00:45:58 +0600

    Start exploring pdo

---
+++ pdo_todo/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a267446d0e2911539129bb8a04c33c88e6910d0f',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a267446d0e2911539129bb8a04c33c88e6910d0f',
+    ),
+  ),
+);
