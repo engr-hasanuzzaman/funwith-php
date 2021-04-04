677d5ca25c2e96bd8e6c79f6f3aafdcf55ced607
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-04-04 20:06:02 +0600

    Use service object with the controller

--- charbd/app/Http/Controllers/TodoController.php
+++ charbd/app/Http/Controllers/TodoController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Todo;
+use App\Services\TodoService;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
+
+    protected $todoService;
+
+    public function __construct(TodoService $todoService) {
+        $this->todoService = $todoService;
+    }
     public function store(Request $request)
     {
         $todo = Todo::create($request->only('title', 'description'));
