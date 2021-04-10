301d2b264ad578b36b9e4dd78e6827e5c8a1d97a
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-04-10 23:06:44 +0600

    Rename the test file properly for auto picking by phpunit

---
+++ UnitTesting/tests/MultipleDependenciesTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class MultipleDependenciesTest extends TestCase
+{
+    public function testFirstDependency(): string
+    {
+        $this->assertTrue(true);
+
+        return 'first';
+    }
+
+    public function testSecondDependency(): string
+    {
+        $this->assertTrue(true);
+
+        return 'second';
+    }
+
+    /**
+     * @depends testSecondDependency
+     * @depends testFirstDependency
+     * */ 
+    public function testMultipleDependencies(string $second, string $first): void
+    {
+        $this->assertSame($first, 'first');
+        $this->assertSame($second, 'second');
+    }
+}
