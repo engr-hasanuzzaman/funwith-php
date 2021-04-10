66d2f34d08e6fda5919d51c4d8f7402ca25225d1
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-04-10 23:59:57 +0600

    Test empty queue

--- UnitTesting/tests/QueueTest.php
+++ UnitTesting/tests/QueueTest.php
@@ -1,1 +1,18 @@
 <?php
+use Queue;
+use PHPUnit\Framework\TestCase;
+
+class QueueTest extends TestCase
+{
+    protected $queue;
+
+    protected function setUp(): void
+    {
+        $this->queue = new Queue;    
+    }
+
+    public function testInitialSizeIsZero()
+    {
+        $this->assertEquals(count($this->queue), 0);
+    }
+}
