177a219add2e6f862256208a0e9ae78224e37014
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-04-12 00:45:33 +0600

    Mocking with mockery

--- UnitTesting/tests/OrderTest.php
+++ UnitTesting/tests/OrderTest.php
@@ -3,6 +3,8 @@
 use App\Order;
 use PHPUnit\Framework\TestCase;
 
+use function PHPUnit\Framework\once;
+
 class OrderTest extends TestCase
 {
     public function testProcess(): void
@@ -13,9 +15,21 @@ class OrderTest extends TestCase
             ->getMock();
 
         // stubbing the pay method that will receive 1.0 as the parameter and return true
-        $paymenService->method('pay')
+        $paymenService->expects(once())
+            ->method('pay')
             ->with($this->equalTo(1.0))
             ->willReturn(true);
+        $order = new Order($paymenService);
+        $order->setPrice(1);
+        $this->assertTrue($order->process());
+    }
+
+    public function testProcessWithMockery()
+    {
+        $paymenService = Mockery::mock('PaymentService');
+        $paymenService->shouldReceive('pay')
+            ->times(1)
+            ->andReturn(true);
         $order = new Order($paymenService);
         $order->setPrice(1);
         $this->assertTrue($order->process());
