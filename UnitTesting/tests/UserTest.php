ddcff7535467ee4f8c95289811b0ae66537491f4
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-04-11 18:39:27 +0600

    Test a method will be called once with two expected methods

--- UnitTesting/tests/UserTest.php
+++ UnitTesting/tests/UserTest.php
@@ -4,6 +4,7 @@ use App\Mailer;
 use App\User;
 use PHPUnit\Framework\TestCase;
 
+use function PHPUnit\Framework\never;
 use function PHPUnit\Framework\once;
 
 class UserTest extends TestCase
@@ -12,7 +13,11 @@ class UserTest extends TestCase
     {
         $mailer = $this->createMock(Mailer::class);
         // $mailer->expects(once())->method('sendEmail')->willReturn(true) -> sendEmail will call once
-        $mailer->expects(once())->method('sendEmail')->willReturn(true);
+        // $mailer->expects(never())->method('sendEmail')->willReturn(true); will failed
+        $mailer->expects(once())
+               ->method('sendEmail')
+               ->with($this->equalTo('test@gm.com'), $this->equalTo('Message'))
+               ->willReturn(true);
         $user = new User('test@gm.com');
         $user->setMailer($mailer);
         $this->assertTrue($user->notify());
