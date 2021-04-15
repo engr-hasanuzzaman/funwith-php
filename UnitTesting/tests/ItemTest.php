2327fc7c53bc4215da7e4fa38f28d796a8f60e2a
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-04-15 21:17:30 +0600

    Start writting TDD for item

---
+++ UnitTesting/tests/ItemTest.php
@@ -0,0 +1,12 @@
+<?php
+use App\Item;
+use PHPUnit\Framework\TestCase;
+
+class ItemTest extends TestCase
+{
+    public function testNewItemHasEmptyTitle()
+    {
+        $item = new Item;
+        $this->assertSame($item->title, null);
+    }
+}
