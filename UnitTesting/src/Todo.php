399c84dfdb9550789cd85d598c8aa7977b6de3f9
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-04-18 15:44:14 +0600

    Create an todo class for testing private and protected function

---
+++ UnitTesting/src/Todo.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+class Todo
+{
+    private int $id;
+    protected String $title;
+
+    public function __construct(int $id = 0, string $title = '') {
+        $this->id = $id;
+        $this->title = $title;
+    }
+
+    private function getId()
+    {
+      return $this->id;
+    }
+
+    protected function getTitle()
+    {
+        return $this->title;
+    }
+}
